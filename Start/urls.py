bdcfb8e0aa4e75f0f59042b69147638702f0d0ee
Yoav <yoavtubay@MacBook-Neo-de-Yoav.local>
2026-06-02 13:00:31 -0500

    mi Primer Commit

---
+++ Start/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('', views.hola, name='home'),
+    path('contact/',views.contact, name='contact'),
+    path('courses/',views.courses, name='courses'),
+    path('elements/',views.elements, name='elements'),
+    path('news/',views.news, name='news'),
+    path('login/',views.login_view, name='login'),
+    path('teachers/',views.teachers, name='teachers')
+    
+ 
+]
---
+++ Start/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
+def hola(request):
+# Create your views here.
+   return render(request, 'index.html')
+def contact(request):
+   return render(request, 'contact.html')
+def login_view(request):
+   return render(request, 'login.html')
+def courses(request):
+   return render(request, 'courses.html')
+def elements(request):
+   return render(request, 'elements.html')
+def news(request):
+   return render(request, 'news.html')
+def teachers(request):
+   return render(request, 'teachers.html')
+def login_view(request):
+    mensaje= ''
+    if request.method== 'POST':
+        username= request.POST.get('username')
+        password= request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request,user)
+        else:
+            mensaje= 'Usuario o contraseña incorrectos'
+    return render (request, 'login.html', {'mensaje': mensaje})

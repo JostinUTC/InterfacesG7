883664d8579339c7d996ebf326a4d6ddb1ef8a67
HeidyUTC <heidy.quinatoa7991@utc.edu.ec>
2026-06-10 12:16:08 -0500

    Agregamos dashboard y mejoramos los logins

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\USUARIO\OneDrive\Desktop\Heidy_interfaces\InterfacesG7\venv"
+set "VIRTUAL_ENV=C:\interfaces\InterfacesG7\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 

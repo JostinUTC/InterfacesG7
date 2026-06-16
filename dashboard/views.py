66dd490ab510afe7b48bf673947cb5ea164dc214
SebasUtc <110674261+SebasUtc@users.noreply.github.com>
2026-06-16 13:08:25 -0500

    crear usuarios ultimos detalles

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
+from pyexpat.errors import messages
+
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login as auth_login
+
 
 def dashboard(request):
    return render(request, 'private/dashboard.html')
@@ -11,3 +14,22 @@ def listar_usuarios(request):
          'usuarios': usuarios
    }
    return render(request, 'private/listar_usuarios.html', contexto)
+def crear_usuario(request):
+   if request.method == 'post':
+      username = request.POST.get('username')
+      email = request.POST.get('email')
+      password = request.POST.get('password')
+      #si no existe el usuario
+      if User.object.filter(username=username).exists():
+         messages.error(request, 'El usuario ya existe')
+         return render (request, 'private/crear_usuario.html')
+      #si no existe el correo
+      if User.object.filter(email=email).exists():
+         messages.error(request, 'El correo ya existe')
+         return render (request, 'private/crear_usuario.html')
+      #crear usuario
+      User.objects.create_user(username=username,email=email,password=password)
+      messages.success(request, 'Usuario creado exitosamente')
+      return render(request, 'private/listar_usuarios.html')
+   return render(request, 'private/crear_usuario.html')
+   

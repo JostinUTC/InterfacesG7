0900798cacf11f1fec8e3b28b7e3d7546bbf6328
SebasUtc <110674261+SebasUtc@users.noreply.github.com>
2026-06-23 12:48:23 -0500

    databases y modelos jostin

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Materia(models.Model):
+    nombre_materia = models.CharField(max_length=100)
+    precio_materia = models.DecimalField(max_digits = 10, decimal_places = 2, verbose_name='precio')
+    creditos_materia = models.IntegerField()
+    num_estudiantes_materia = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre_materia
+
+

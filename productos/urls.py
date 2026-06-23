92d6d0a18cd0ae0b9d41757273b7bbab00b9dfd8
SebasUtc <110674261+SebasUtc@users.noreply.github.com>
2026-06-23 13:19:25 -0500

    Finalizacion de Crear materia

--- productos/urls.py
+++ productos/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.listar_materias, name='listar_materias'),
+    path('crear_materias/', views.crear_materias, name='crear_materias'),
 ]

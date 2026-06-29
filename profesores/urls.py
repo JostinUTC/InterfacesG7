86e40a765a5144fb23c3398fc1bc650c6faa128e
HeidyUTC <heidy.quinatoa7991@utc.edu.ec>
2026-06-29 09:02:58 -0500

    editar y eliminara boton del crud

--- profesores/urls.py
+++ profesores/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.listar_profesores, name='listar_profesores'),
-    path('crear_materias/', views.crear_materias, name='crear_materias'),
+    path('crear_profesor/', views.crear_profesor, name='crear_profesor'),
     path('eliminar_materia/<int:id>/', views.eliminar_materia, name='eliminar_materia'),
     path('editar_materia/<int:id>/', views.editar_materia, name='editar_materia')
 ]
--- profesores/views.py
+++ profesores/views.py
@@ -11,7 +11,7 @@ def crear_profesor(request):
       curso_profesor = request.POST.get("cursoProfesor")
     
 
-      Profesor.objects.create(
+      Profesores.objects.create(
          nombre_profesor = nombre_profesor,
          sueldo_profesor = sueldo_profesor ,
          curso_profesor = curso_profesor,
@@ -20,3 +20,20 @@ def crear_profesor(request):
       messages.success(request, 'Profesor añadido exitosamente')
       return redirect('listar_profesores')
    return render(request, 'profesores/crear_profesor.html')
+def eliminar_profesor(request, id):
+   profesores = profesores.objects.get(id=id)
+   profesores.delete()
+   profesores.success(request, 'profesor eliminado exitosamente')
+   return redirect('listar_profesores')
+def editar_profesore(request, id):
+   profesores = profesores.objects.get(id=id)
+   if request.method == 'POST':
+      profesores.nombre_profesor = request.POST.get("nombreProfesor")
+      profesores.sueldo_profesor = request.POST.get("sueldoProfesor")
+      profesores.curso_profesor = request.POST.get("cursoProfesor")
+   
+
+      profesores.save()
+      messages.success(request, 'La materia se actualizo con exito')
+      return redirect('listar_profesores')
+   return render(request, 'profesor/editar_profesor.html', {'profesores': profesores})
